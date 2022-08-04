5ff6ccf0fa37d6e5c5523661f6af5ef7a230fabf
brs14shn <brsshn292929@gmail.com>
2022-08-04 16:35:28 +0300

    carousel updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,74 +1,62 @@
-import React from "react";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import React, { useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { hotelCards } from "../utils/hotelCard";
 
 const Carousel = () => {
+  const [sliderRef, setSliderRef] = useState(null);
   const sliderSettings = {
-    slidesToShow: 3,
+    arrows: false,
+    slidesToShow: 2,
+    //! Görünümde tutulacak slayt sayısını belirlemek için
     slidesToScroll: 1,
+    //! Caruselde gezinirken hareket ettirilecek slayt sayısını belirlemek için
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
-
-  const hotelCards = [
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1559508551-44bff1de756b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
-      title: "Studio Room",
-      description: "Lorem ipsum dolor sit amet, consectur dolori",
-      pricingText: "USD 50/Day",
-      features: ["Free Wifi", "Free breakfast"],
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1616940844649-535215ae4eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-      title: "Deluxe Room",
-      description: "Lorem ipsum dolor sit amet, consectur dolori",
-      pricingText: "USD 80/Day",
-      features: ["Free Wifi", "Free breakfast"],
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1599619351208-3e6c839d6828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
-      title: "King Deluxe Room",
-      description: "Lorem ipsum dolor sit amet, consectur dolori",
-      pricingText: "USD 150/Day",
-      features: ["Free Wifi", "Free breakfast", "Discounted Meals"],
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1461092746677-7b4afb1178f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      title: "Royal Suite",
-      description: "Lorem ipsum dolor sit amet, consectur dolori",
-      pricingText: "USD 299/Day",
-      features: [
-        "Free Wifi",
-        "Free breakfast",
-        "Discounted Meals",
-        "MacBook for work use (hotel's property)",
-      ],
-    },
-  ];
 
   return (
     <div className="content">
-      <Slider {...sliderSettings}>
+      <div className="controls">
+        <button onClick={sliderRef?.slickPrev}>
+          <FaChevronLeft />
+        </button>
+        <button onClick={sliderRef?.slickNext}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <Slider ref={setSliderRef} {...sliderSettings}>
         {hotelCards.map((card, index) => (
-          <div key={index} className="">
-            <h2 className="">{card.title}</h2>
-            <img
-              className=""
-              src={card.imageSrc}
-              alt={card.title}
-              width="100"
-            />
-            <p>{card.description}</p>
-            <ul>
-              {card.features.map((feature, index) => (
-                <li key={index}>{feature}</li>
-              ))}
-            </ul>
-            <button className="">Buy Now</button>
+          <div key={index} className="card">
+            <img src={card.imageSrc} alt={card.title} className="card-image" />
+            <div className="text-info">
+              <div className="card-header">
+                <h2>{card.title}</h2>
+                <span>{card.pricingText}</span>
+              </div>
+              <p>{card.description}</p>
+              <ul>
+                {card.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))}
+              </ul>
+            </div>
+            <button>Buy Now</button>
           </div>
         ))}
       </Slider>
